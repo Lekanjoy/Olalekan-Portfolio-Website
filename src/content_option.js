6ae05c1bec9b92e8a9984365385a5c27f79fca03
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2023-08-02 10:01:49 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,115 @@
+const logotext = "OLALEKAN";
+const meta = {
+    title: "Olalekan  Alabi",
+    description: "I’m Olalekan  Alabi, a Frontend Developer",
+};
+
+const introdata = {
+    title: "I’m Olalekan  Alabi",
+    animated: {
+        first: "I love creating cool web interfaces ",
+        second: "I code responsive websites",
+        third: "I develop webapps",
+    },
+    description: "I'm Olalekan, a frontend developer with a passion for creating beautiful and responsive interfaces that enhance user experience.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Hello👋🏻,",
+    aboutme: " I'm Olalekan, a frontend developer with a passion for creating beautiful and responsive interfaces that enhance user experience. I have experience using HTML, CSS, and JavaScript to develop interactive websites and applications. I'm comfortable working with popular frameworks such as React and Vue. As a recent graduate, I'm eager to continue expanding my knowledge and skills in frontend development, and I'm excited about the opportunity to collaborate with experienced developers to learn best practices and improve my craft. I'm excited about the opportunity to bring my skills and enthusiasm to a junior developer or internship role, and to work with a team to create exceptional digital experiences for users.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week", 
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML",
+    },
+    {
+        name: "CSS",
+    },
+    {
+        name: "Javascript",
+    },
+    {
+        name: "Typescript",
+    },
+    {
+        name: "Reactjs / Nextjs",
+    },
+    {
+        name: "TailwindCSS",
+    },  
+    {
+        name: "Git",
+    },
+    {
+        name: "Github",
+    },
+];
+
+
+const dataportfolio = [{
+        url: "https://entertainment-webapp.vercel.app/",
+        description: "Introducing MovieMagic: Your hub for trending, new releases, and personalized movie recommendations. Discover the magic of cinema with our authentic entertainment web app, featuring secure authentication for a seamless movie experience. ",
+        code: "https://github.com/Lekanjoy/Entertainment-Webapp"
+    },
+    {
+        url: "https://olalekan-store.netlify.app/",
+        description: "Shop Smart, Shop Swift: Your Mini Ecommerce Destination. Browse products with seamless previews, create personalized profiles, and experience hassle-free checkout and secure payment options. Elevate your shopping journey with our platform designed for convenience and confidence. 🛍️💳",
+        code: "https://github.com/Lekanjoy/Fashion-commerce"
+    },
+    {
+        url: "https://scissor-app-next.vercel.app/",
+        description: "Simplify, Share, and Secure: Your Ultimate Link Shortener. Unlock the power to condense URLs, generate QR codes, copy, edit, and manage links with ease. Elevate your online experience with our authenticated platform designed for efficiency and control. 🔗🚀",
+        code: "https://github.com/Lekanjoy/Scissors-App"
+    },
+    {
+        url: "https://rest-countries-api-bbf667.netlify.app/",
+        description: "Explore the World: Your Passport to Global Information. Uncover fascinating details about every country, filter by continents, and search for specific destinations. Journey through our interactive website and embrace the diversity of our planet. 🌍✈️",
+        code: "https://github.com/Lekanjoy/REST-Countries-API-Stats"
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "alabilekanemmanuel@gmail.com",
+    YOUR_FONE: "(+234)81-018-537-01",
+    description: "We'd love to hear from you! Whether you have a question, feedback, or just want to say hello, this is the place to get in touch. Drop us a message using the form below, and we'll get back to you as soon as possible.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Lekanjoy",
+    linkedin: "https://www.linkedin.com/in/olalekan-alabi-b3740a220/",
+    twitter: "https://twitter.com/Olalaykun",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
